a65c8cd1d50adf10c89e24e28752fd23a423ba81
Cristian Kiss <cristian.kiss@nearform.com>
2015-10-26 14:58:18 +0200

    formatting

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-var chalk = require('chalk');
+var chalk = require('chalk')
 
-function s(n) {
-  return Array.apply(null, {length: n}).join(' ');
+function s (n) {
+  return Array.apply(null, {length: n}).join(' ')
 }
 
-function logo(opts) {
-  opts = (opts) ? opts : {}
+function logo (opts) {
+  opts = opts || {}
   opts.leftPadding = typeof opts.leftPadding === 'number'
     ? opts.leftPadding
-    : (process.stdout.columns / 2) - 4;
-  opts.text = (opts.text) ? opts.text : '';
+    : (process.stdout.columns / 2) - 4
+  opts.text = (opts.text) ? opts.text : ''
 
-  return              "\n"+
-    chalk.gray ("\n"+s(opts.leftPadding)+" ###############") +
-    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("###############") + chalk.gray("#") +
-    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("###############") + chalk.gray("#") +
-    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("###") + chalk.gray("#################") +
-    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("###") + chalk.gray("#") + chalk.white("################") + chalk.gray("#") +
-    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("###") + chalk.gray("#") + chalk.white("################") + chalk.gray("#") +
-    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("###") + chalk.gray("#") + chalk.white("####") + chalk.gray("#####") + chalk.white("#######") + chalk.gray("#") +
-    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("###") + chalk.gray("#") + chalk.white("####") + chalk.gray("#") + chalk.white("####") + chalk.gray("##") + chalk.white("#####") + chalk.gray("#") +
-    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("###") + chalk.gray("#") + chalk.white("####") + chalk.gray("#") + chalk.white("#####") + chalk.gray("##") + chalk.white("####") + chalk.gray("#    ") + chalk.cyan(opts.text) +
-    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("###") + chalk.gray("#") + chalk.white("####") + chalk.gray("#") + chalk.white("####") + chalk.gray("##") + chalk.white("#####") + chalk.gray("#") +
-    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.gray("#############") + chalk.white("#######") + chalk.gray("#") +
-    chalk.gray ("\n"+s(opts.leftPadding)+"    ") + chalk.gray("#") + chalk.white("################") + chalk.gray("#") +
-    chalk.gray ("\n"+s(opts.leftPadding)+"    ") + chalk.gray("#") + chalk.white("###############") + chalk.gray("#") +
-    chalk.gray ("\n"+s(opts.leftPadding)+"    ") + chalk.gray("#") + chalk.gray("##############") + chalk.gray("#") +
-    "\n\n";
+  return '\n' +
+  chalk.gray('\n' + s(opts.leftPadding) + ' ###############') +
+  chalk.gray('\n' + s(opts.leftPadding) + '#') + chalk.cyan('###############') + chalk.gray('#') +
+  chalk.gray('\n' + s(opts.leftPadding) + '#') + chalk.cyan('###############') + chalk.gray('#') +
+  chalk.gray('\n' + s(opts.leftPadding) + '#') + chalk.cyan('###') + chalk.gray('#################') +
+  chalk.gray('\n' + s(opts.leftPadding) + '#') + chalk.cyan('###') + chalk.gray('#') + chalk.white('################') + chalk.gray('#') +
+  chalk.gray('\n' + s(opts.leftPadding) + '#') + chalk.cyan('###') + chalk.gray('#') + chalk.white('################') + chalk.gray('#') +
+  chalk.gray('\n' + s(opts.leftPadding) + '#') + chalk.cyan('###') + chalk.gray('#') + chalk.white('####') + chalk.gray('#####') + chalk.white('#######') + chalk.gray('#') +
+  chalk.gray('\n' + s(opts.leftPadding) + '#') + chalk.cyan('###') + chalk.gray('#') + chalk.white('####') + chalk.gray('#') + chalk.white('####') + chalk.gray('##') + chalk.white('#####') + chalk.gray('#') +
+  chalk.gray('\n' + s(opts.leftPadding) + '#') + chalk.cyan('###') + chalk.gray('#') + chalk.white('####') + chalk.gray('#') + chalk.white('#####') + chalk.gray('##') + chalk.white('####') + chalk.gray('#    ') + chalk.cyan(opts.text) +
+  chalk.gray('\n' + s(opts.leftPadding) + '#') + chalk.cyan('###') + chalk.gray('#') + chalk.white('####') + chalk.gray('#') + chalk.white('####') + chalk.gray('##') + chalk.white('#####') + chalk.gray('#') +
+  chalk.gray('\n' + s(opts.leftPadding) + '#') + chalk.gray('#############') + chalk.white('#######') + chalk.gray('#') +
+  chalk.gray('\n' + s(opts.leftPadding) + '    ') + chalk.gray('#') + chalk.white('################') + chalk.gray('#') +
+  chalk.gray('\n' + s(opts.leftPadding) + '    ') + chalk.gray('#') + chalk.white('###############') + chalk.gray('#') +
+  chalk.gray('\n' + s(opts.leftPadding) + '    ') + chalk.gray('#') + chalk.gray('##############') + chalk.gray('#') +
+  '\n\n'
 }
 
 logo.toTTY = function (opts) {
-  return process.stdout.write(logo(opts));
+  return process.stdout.write(logo(opts))
 }
 
-module.exports = logo.toTTY({text: "DECK"});
+module.exports = logo
