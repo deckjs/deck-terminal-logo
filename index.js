bdfd666d615d1387b729c81ffda0f3067e79d595
Cristian Kiss <cristian.kiss@nearform.com>
2015-10-26 12:48:05 +0200

    initial ascii logo

---
+++ index.js
@@ -0,0 +1,33 @@
+var chalk = require('chalk');
+
+function s(n) {
+  return Array.apply(null, {length: n}).join(' ');
+}
+
+function logo(opts) {
+  opts = (opts) ? opts : {}
+  opts.leftPadding = typeof opts.leftPadding === 'number'
+    ? opts.leftPadding
+    : (process.stdout.columns / 2) - 4;
+  opts.text = (opts.text) ? opts.text : '';
+
+  return              "\n"+
+    chalk.gray ("\n"+s(opts.leftPadding)+" #############") +
+    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("#############") + chalk.gray("#") +
+    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("##") + chalk.gray("###############") +
+    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("##") + chalk.gray("#") + chalk.white("##############") + chalk.gray("#") +
+    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("##") + chalk.gray("#") + chalk.white("###") + chalk.gray("#####") + chalk.white("######") + chalk.gray("#    ") + opts.text +
+    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("##") + chalk.gray("#") + chalk.white("###") + chalk.gray("#") + chalk.white("####") + chalk.gray("##") + chalk.white("####") + chalk.gray("#") +
+    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("##") + chalk.gray("#") + chalk.white("###") + chalk.gray("#") + chalk.white("#####") + chalk.gray("##") + chalk.white("###") + chalk.gray("#") +
+    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.cyan("##") + chalk.gray("#") + chalk.white("###") + chalk.gray("#") + chalk.white("####") + chalk.gray("##") + chalk.white("####") + chalk.gray("#") +
+    chalk.gray ("\n"+s(opts.leftPadding)+"#") + chalk.gray("###########") + chalk.white("######") + chalk.gray("#") +
+    chalk.gray ("\n"+s(opts.leftPadding)+"   ") + chalk.gray("#") + chalk.white("##############") + chalk.gray("#") +
+    chalk.gray ("\n"+s(opts.leftPadding)+"   ") + chalk.gray("#") + chalk.gray("#############") + chalk.gray("#") +
+    "\n\n";
+}
+
+logo.toTTY = function (opts) {
+  return process.stdout.write(logo(opts));
+}
+
+module.exports = logo.toTTY({text: "Deck"});
